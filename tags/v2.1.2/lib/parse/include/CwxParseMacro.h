36c1289981014bdf2df32fa9690c7ca2c3bedbee
cwinux <cwinux@edd276ac-0504-11df-9339-7f1b9139ac79>
2011-08-28 22:38:09 +0000

    create tagv2.1.2

---
+++ tags/v2.1.2/lib/parse/include/CwxParseMacro.h
@@ -0,0 +1,23 @@
+#ifndef __CWX_PARSE_MACRO_H__
+#define __CWX_PARSE_MACRO_H__
+/*
+版权声明：
+    本软件遵循GNU GPL V3（http://www.gnu.org/licenses/gpl.html），
+    联系方式：email:cwinux@gmail.com；微博:http://t.sina.com.cn/cwinux
+*/
+
+
+/**
+@file CwxParseMacro.h
+@brief Parse模块的公共宏定义文件。
+@author cwinux@gmail.com
+@version 0.1
+@date 2009-10-10
+@warning
+@bug
+*/
+#include "CwxPre.h"
+
+#include "CwxPost.h"
+
+#endif
