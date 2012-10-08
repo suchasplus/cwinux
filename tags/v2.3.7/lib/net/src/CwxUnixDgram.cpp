1ff67e6a213cc91f217f04d8eac0d6014387de06
cwinux <cwinux@edd276ac-0504-11df-9339-7f1b9139ac79>
2012-10-08 11:43:55 +0000

    create new tag

---
+++ tags/v2.3.7/lib/net/src/CwxUnixDgram.cpp
@@ -0,0 +1,18 @@
+﻿#include "CwxUnixDgram.h"
+
+CWINUX_BEGIN_NAMESPACE
+
+CwxUnixDgram::CwxUnixDgram()
+{
+
+}
+
+CwxUnixDgram::CwxUnixDgram(CWX_HANDLE handle):CwxSockDgram(handle)
+{
+}
+CwxUnixDgram::~CwxUnixDgram(void)
+{
+
+}
+
+CWINUX_END_NAMESPACE
